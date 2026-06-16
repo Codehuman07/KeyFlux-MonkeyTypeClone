7267f9709f9203d96e9c4fe16d9bfdc1e40c5167
Yogye Berry <yogyeberry@gmail.com>
2026-06-16 19:46:15 +0530

    Completed landing page

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -1,12 +1,33 @@
-import React from 'react'
-import Background from "../components/Background"
+import React from "react";
+import Background from "../components/Background";
+import BorderGlow from "../components/BorderGlow";
 
 function Landing() {
     return (
-        <>
-            <Background/>
-        </>
-    )
+        <Background>
+            <div className="relative z-10 flex min-h-screen flex-col items-center justify-center gap-6 text-center">
+                <h1 className=" text-4xl text-white">Welcome to KeyFlux</h1>
+                <p className="text-lg text-gray-300">
+                    Test your typing speed and accuracy!
+                </p>
+                <BorderGlow
+                    edgeSensitivity={30}
+                    glowColor="40 80 80"
+                    backgroundColor="#120F17"
+                    borderRadius={28}
+                    glowRadius={40}
+                    glowIntensity={1}
+                    coneSpread={25}
+                    animated={false}
+                    colors={['#c084fc', '#f472b6', '#38bdf8']}
+                >
+                    <div className="p-8 text-white cursor-pointer">
+                        <h2>Start Typing!!!</h2>
+                    </div>
+                </BorderGlow>
+            </div>
+        </Background>
+    );
 }
 
-export default Landing
+export default Landing;
--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,24 +1,45 @@
 import React from 'react'
 import Particles from '../components/Particles'
 
-function Background() {
+function Background({ children }) {
     return (
-        <>
-            <div style={{ width: '100%', height: '100vh', position: 'relative' }}>
-            <Particles
-                particleColors={["#ffffff"]}
-                particleCount={800}
-                particleSpread={20}
-                speed={0.1}
-                particleBaseSize={50}
-                moveParticlesOnHover
-                alphaParticles={false}
-                disableRotation={true}
-                pixelRatio={1}
-            />
+        <div
+            style={{
+                width: '100%',
+                height: '100vh',
+                position: 'relative',
+            }}
+        >
+            <div
+                style={{
+                    position: 'absolute',
+                    inset: 0,
+                    zIndex: 0,
+                }}
+            >
+                <Particles
+                    particleColors={["#ffffff"]}
+                    particleCount={800}
+                    particleSpread={20}
+                    speed={0.1}
+                    particleBaseSize={50}
+                    moveParticlesOnHover
+                    alphaParticles={false}
+                    disableRotation={true}
+                    pixelRatio={1}
+                />
             </div>
-        </>
-    )
+
+            <div
+                style={{
+                    position: 'relative',
+                    zIndex: 1,
+                }}
+            >
+                {children}
+            </div>
+        </div>
+    );
 }
 
 export default Background
