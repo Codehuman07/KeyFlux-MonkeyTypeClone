acb32505b450b5e30e944a3e774daa06ff64b517
Yogye Berry <yogyeberry@gmail.com>
2026-06-19 05:15:32 +0530

    feat: enhance Typing component with accuracy calculation and visual feedback

--- src/Pages/Typing.jsx
+++ src/Pages/Typing.jsx
@@ -1,32 +1,57 @@
 import React, { useEffect, useState } from 'react'
 function Typing() {
-    const[timer, setTimer] = useState(60)
-    const[textinput, setTextinput] = useState("")
-    const[para,setPara] = useState("The quick brown fox jumped over the lazy dog.")
-    function onchange(e){
+    const [timer, setTimer] = useState(60)
+    const [textinput, setTextinput] = useState("")
+    const [started, setStarted] = useState(false)
+    const [para, setPara] = useState("The quick brown fox jumped over the lazy dog.")
+    const [wpm, setwpm] = useState(0)
+    const correct = para.split("").filter((char, index) => {
+        return textinput[index] === char
+    }).length
+    const wrong = textinput.length - correct
+    let accuracy = 0
+    if(correct + wrong > 0){
+        accuracy = (correct / (correct + wrong)) * 100
+    }
+    function onchange(e) {
+        if (started === false) {
+            setStarted(true)
+        }
         setTextinput(e.target.value)
     }
     useEffect(() => {
         const time = setTimeout(() => {
-            if (timer > 0) {
+            if (timer > 0 && started === true) {
                 setTimer(timer - 1)
             }
         }, 1000)
         return () => clearTimeout(time)
-    }, [timer])
+    }, [timer, started])
     return (
         <div className='z-10 min-h-screen flex flex-col items-center'>
             <h1 className='text-6xl text-white pt-8'>KeyFlux</h1>
             <div className='flex-1 flex flex-col items-center justify-center w-full gap-12'>
                 <div className='border-2 border-white flex justify-around items-center p-5 w-[80%]'>
                     <h3 className='text-white'>Time: {timer}</h3>
-                    <h3 className='text-white'>WPM: </h3>
-                    <h3 className='text-white'>Accuracy: </h3>
+                    <h3 className='text-white'>WPM: {wpm}</h3>
+                    <h3 className='text-white'>Accuracy: {accuracy.toFixed(1)}%</h3>
                 </div>
                 <div className='border-2 border-white flex justify-around items-center p-10 w-[80%]'>
-                    <h1 className='text-white text-3xl'>{para}</h1>
+                    <h1 className='text-white'>{para.split("").map((char, index) => {
+                            let color = 'text-white'
+                            if (index < textinput.length) {
+                                if (textinput[index] === char) {
+                                    color = 'text-green-500'
+                                } else {
+                                    color = 'text-red-500'
+                                }
+                            }
+                            return (
+                                <span className={color} key={index}>{char}</span>)
+                        })}
+                    </h1>
                 </div>
-                <input type="text" value={textinput} onChange={onchange} autoFocus className='w-0 h-0 opacity-0'></input>
+                <input type="text" value={textinput} onChange={onchange} autoFocus className='w-0 h-0 opacity-0'/>
             </div>
         </div>
     )
