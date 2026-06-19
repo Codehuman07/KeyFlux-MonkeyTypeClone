8795b4420987f9a884e6d9a77556bd354b605c24
Yogye Berry <yogyeberry@gmail.com>
2026-06-19 19:27:50 +0530

    feat: implement dynamic paragraph generation for Typing component

--- src/Pages/Typing.jsx
+++ src/Pages/Typing.jsx
@@ -1,17 +1,29 @@
 import React, { useEffect, useState } from 'react'
+import { paragrphs } from '../components/paragraphs'
 function Typing() {
     const [timer, setTimer] = useState(60)
     const [textinput, setTextinput] = useState("")
     const [started, setStarted] = useState(false)
-    const [para, setPara] = useState("The quick brown fox jumped over the lazy dog.")
-    const [wpm, setwpm] = useState(0)
+    const [para, setPara] = useState("")
+    const timeUsed = 60 - timer
+    function paragraphgenerator(){
+        const numb = Math.floor(Math.random()*10)
+        setPara(paragrphs[numb])
+    }
+    useEffect(()=>{
+        paragraphgenerator()
+    },[])
+    let wpm = 0
     const correct = para.split("").filter((char, index) => {
         return textinput[index] === char
     }).length
     const wrong = textinput.length - correct
     let accuracy = 0
-    if(correct + wrong > 0){
-        accuracy = (correct / (correct + wrong)) * 100
+    if (correct + wrong > 0){
+        accuracy = (correct/(correct + wrong))*100
+    }
+    if (timeUsed > 0) {
+        wpm = Math.round((correct / 5) / (timeUsed / 60))
     }
     function onchange(e) {
         if (started === false) {
@@ -38,20 +50,20 @@ function Typing() {
                 </div>
                 <div className='border-2 border-white flex justify-around items-center p-10 w-[80%]'>
                     <h1 className='text-white'>{para.split("").map((char, index) => {
-                            let color = 'text-white'
-                            if (index < textinput.length) {
-                                if (textinput[index] === char) {
-                                    color = 'text-green-500'
-                                } else {
-                                    color = 'text-red-500'
-                                }
+                        let color = 'text-white'
+                        if (index < textinput.length) {
+                            if (textinput[index] === char) {
+                                color = 'text-green-500'
+                            } else {
+                                color = 'text-red-500'
                             }
-                            return (
-                                <span className={color} key={index}>{char}</span>)
-                        })}
+                        }
+                        return (
+                            <span className={color} key={index}>{char}</span>)
+                    })}
                     </h1>
                 </div>
-                <input type="text" value={textinput} onChange={onchange} autoFocus className='w-0 h-0 opacity-0'/>
+                <input type="text" value={textinput} onChange={onchange} autoFocus className='w-0 h-0 opacity-0' />
             </div>
         </div>
     )
